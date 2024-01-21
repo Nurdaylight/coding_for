0f3afaa129fe52630efa8064004981b49cc4d64a
Nurdaylight <megnlibayevnurdaulet@student.ceu.edu>
2024-01-21 18:01:07 +0000

    added py file

---
+++ main.py
@@ -0,0 +1,38 @@
+print(2+4)
+print (type(10/4))
+x=5
+y=x**5
+print (y)
+print (type(y))
+a="hi"
+print(a*4)
+
+b='HEHEHE'
+print(a,' ', b)
+print(a[0:4])
+print(f"our string has {len(a) } characters")
+print(2==2)
+print (True+ True)
+#Multiple logical statements
+print(1==1 and 1==2)
+print(1==1 or 1==1)
+
+myList=[1,2,3,4,5,6,7,8,9,10]
+print(type(myList))# <classe 'list'>
+print(myList[0:5])#1
+print(  len(myList))
+
+print(f"our lists has {len(myList) } elements")
+
+mixList =  [1,2, myList]
+print(mixList)
+mixList.append(6)
+mixList.pop()
+print(mixList)
+
+
+
+
+
+
+
